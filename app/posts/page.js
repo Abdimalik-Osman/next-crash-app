4a3fb49e86a517a3f503a7a4b76f05de03690e4d
Abdimalik-Osman <engabdimalik8818@gmail.com>
2023-12-24 23:29:45 +0300

    get posts added

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,10 +1,25 @@
 
-const Posts = () => {
+// revalidate the page every 1 hour
+
+import Link from "next/link";
+
+// export const revalidate = 3600
+const  Posts =  async() =>{
+  const posts = await fetch('http://localhost:3000/api/post',{cache: 'no-store'});
+  const postData = await posts.json();
   return (
-    <div>
-      posts
-    </div>
+    <>
+   <h1>Hello from the post page</h1>
+    {
+      postData.map((post) =>(
+        <div key={post.id} className="">
+        <Link href={`/posts/${post.id}`} className="list-item">{post.title}</Link>
+        <Link href={`/posts/${post.id}`}>{post.body}</Link>
+        </div>
+      ))
+    }
+  </>
   )
 }
 
-export default Posts
+export default Posts;
