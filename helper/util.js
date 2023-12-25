b75194e0752ac7ceb58dab6cfb95e2cf145949ce
abdiwahab <abdulwahab@gmail.com>
2023-12-25 13:40:41 +0300

    write data function added

--- helper/util.js
+++ helper/util.js
@@ -1,9 +1,17 @@
-import { promises as fs } from "fs";
-import path from "path";
+import { promises as fs } from 'fs';
+import path from 'path';
 
-const dataFilePath = path.join(process.cwd(),'/helper/data.json')
-// read data from JSON file using file system of node js.
-export const readData = async()=>{
+// Path to the JSON file
+const dataFilePath = path.join(process.cwd(), '/helper/data.json');
+
+// Helper function to read data from JSON file
+export async function readData() {
     const jsonData = await fs.readFile(dataFilePath, 'utf8');
-    return JSON.parse(jsonData)
+    return JSON.parse(jsonData);
+}
+
+// Helper function to write data to JSON file
+export async function writeData(data) {
+    const jsonData = JSON.stringify(data, null, 2);
+    await fs.writeFile(dataFilePath, jsonData, 'utf8');
 }
